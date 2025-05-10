4643010dd897bdf6a22f9bede2a73d9f274519a5
Malayappan-ap <135354957+Malayappan-ap@users.noreply.github.com>
2025-05-10 23:55:41 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import AnimatedChatBot from './components/AnimatedChatBot';
+
+function App() {
+  return (
+    <div>
+      <AnimatedChatBot />
+    </div>
+  );
+}
+
+export default App;
